9d75b461e9986774caeec6daef5b6afc9ffbe751
Ivan <vankata294n4@gmail.com>
2022-10-08 16:40:20 +0300

    importing libraries and implementing simple AI logic

---
+++ controller.py
@@ -0,0 +1,34 @@
+import time
+
+import cv2
+import keyboard
+import numpy as np
+from PIL import ImageGrab
+import win32gui
+import win32ui
+import win32con
+import tkinter
+
+
+PylaStart = PylaStart('start')
+
+def play():
+    pass
+
+
+def start():
+    pass
+
+
+count = 0
+
+
+def controlAI():
+
+    while True:
+        start()
+        play()
+
+
+controlAI()
+print("Exit")
