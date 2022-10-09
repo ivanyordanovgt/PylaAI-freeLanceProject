5c19da9b3154065af345bdb6717dc7a6a9def808
Ivan <vankata294n4@gmail.com>
2022-10-09 15:58:53 +0300

    Installing needed libraries

---
+++ vision.py
@@ -0,0 +1,12 @@
+import os
+
+import mouse
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+import numpy as np
+import win32con
+import cv2
+from pynput.keyboard import Key, Controller
